e4b3c745123dfe5ba7550548f6cd59dc3d8e4574
Rubén Pozo <rpozo@lynxview.es>
2019-07-10 07:48:03 +0200

    Testing resolver

--- tests/recipe.test.ts
+++ tests/recipe.test.ts
@@ -1,32 +1,40 @@
 import { GraphQLSchema, graphql } from "graphql";
 import { buildSchema, Resolver, Query, Arg, Int } from "type-graphql";
 import { Recipe } from "../entities/recipe";
+import { User } from "../entities/user";
 
-describe("typeorm-basic-usage unit tests",  () => {
-    let schema: GraphQLSchema;
-    let argsData: any;
-    
-    beforeAll(async () => {
+describe("typeorm-basic-usage unit tests", () => {
+  let schema: GraphQLSchema;
+  let argsData: any;
+  let actualValue: any;
+  let expectedValue: any;
 
-        @Resolver(of => Recipe)
-        class RecipeResolver {
+  beforeAll(async () => {
 
-        
-          @Query(returns => Recipe, { nullable: true })
-          recipe(@Arg("recipeId", type => Int) recipeId: number) {
-            argsData = recipeId;
-            return {};
-          }
-        }
+    @Resolver(of => Recipe)
+    class RecipeResolver {
 
-        schema = await buildSchema({
-            resolvers: [RecipeResolver],
-            validate: true,
-          });
+
+      @Query(returns => Recipe, { nullable: true })
+      recipe(@Arg("recipeId", type => Int) recipeId: number) {
+        argsData = recipeId;
+        const returnValue = new Recipe();
+        returnValue.title = "title";
+        returnValue.description = "description";
+        returnValue.author = new User();
+        returnValue.author.nickname = "jhon";
+        return returnValue;
+      }
+    }
+
+    schema = await buildSchema({
+      resolvers: [RecipeResolver],
+      validate: true,
     });
+  });
 
-    it("recipe query", async () => {
-        const query = `query {
+  it("recipe query", async () => {
+    const query = `query {
             recipe( recipeId: 1 ) {
               title,
               author {
@@ -35,7 +43,19 @@ describe("typeorm-basic-usage unit tests",  () => {
             }
           }
           `;
-        await graphql(schema, query);
+    expectedValue = {
+      "data": {
+        "recipe": {
+          "title": "title",
+          "author": {
+            "nickname": "jhon"
+          }
+        }
+      }
+    };
+    actualValue = await graphql(schema, query);
+    console.log("ActualValue:", actualValue);
     expect(argsData).toEqual(1);
-    });
+    expect(JSON.stringify(expectedValue)).toEqual(JSON.stringify(actualValue));
+  });
 });
